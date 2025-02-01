ab8cff867f674000c6c20884065a713af1896d5d
pablovillazon <p.villazon@gmail.com>
2025-02-01 12:01:16 -0400

    Agregar PS script para monitorear proceso, hacer que la app C++ reciba argumentos, threads y nro de transacciones

--- AppC/pasarela_threadpool.cpp
+++ AppC/pasarela_threadpool.cpp
@@ -106,12 +106,13 @@ public:
     ~Logger(){
         flush();
     }
-    void logTimeElapsed(const std::string& activityName, std::chrono::steady_clock::time_point startTime) {
+    long logTimeElapsed(const std::string& activityName, std::chrono::steady_clock::time_point startTime) {
         auto endTime = std::chrono::steady_clock::now();
         auto timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
         std::ostringstream oss;
         oss << "Tiempo transcurrido en " << activityName << ": " << timeElapsed << " ms";
         log(oss.str());
+        return timeElapsed;
     }
 
 
@@ -123,7 +124,7 @@ public:
         std::ostringstream oss;
         oss << std::put_time(std::localtime(&now_time_t), "%Y-%m-%d %H:%M:%S")
                 << '.' << std::setw(3) << std::setfill('0') << now_ms.count()
-                << " - " << message << std::endl;
+                << " - " << message /*<< std::endl*/;
         messageQueue.push(oss.str());
     }
 
@@ -176,13 +177,34 @@ class PasarelaDePagos
     Logger& logger;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
+    int nroTransacciones = 100;
+    int nroThreads = 4;
+    // Verificar si se ingresaron los argumentos
+    if (argc == 3) {
+        // Intentar convertir los argumentos a enteros
+        try {
+        nroThreads = std::stoi(argv[1]);
+        nroTransacciones = std::stoi(argv[2]);
+        } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: Los argumentos no son números enteros." << std::endl;     
+        return 1;
+    }
+    } else {
+        // Establecer valores por defecto si no se ingresaron argumentos
+        nroTransacciones = 100;
+        nroThreads = 4;
+        std::cout << "No se ingresaron argumentos. Se establecieron valores por defecto: " << nroThreads << " y " << nroTransacciones << std::endl;
+    }
+
+
     Logger logger("log_v2.txt");
     auto startTime = std::chrono::steady_clock::now();
     logger.log("---Inicio de Ejecucion de Transacciones---");
 
-    ThreadPool pool(4); //Inicializar un threadpool con 4 threads
+    
+    ThreadPool pool(nroThreads); //Inicializar un threadpool con 4 threads
     
 
     //Agregar tareas al threadpool
@@ -192,36 +214,8 @@ int main()
 
     //Procesando las transacciones
     PasarelaDePagos pasarela(logger);
-    /*
-    Transaccion transaccion{1, 10.99, "USD"};
-    Transaccion transaccion2{2, 10.99, "USD"};
-    Transaccion transaccion3{3, 10.99, "USD"};
-    Transaccion transaccion4{4, 10.99, "USD"};
-    Transaccion transaccion5{5, 10.99, "USD"};
-    Transaccion transaccion6{6, 12.99, "USD"};
-    Transaccion transaccion7{7, 12.99, "USD"};
-    Transaccion transaccion8{8, 12.99, "USD"};
-    Transaccion transaccion9{8, 12.99, "USD"};
-    //std::cout<<transaccion.moneda<<std::endl;
-    auto future = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion);
-    auto future2 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion2);
-    auto future3 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion3);
-    auto future4 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion4);
-    auto future5 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion5);
-    auto future6 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion6);
-    auto future7 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion7);
-    auto future8 = pool.enqueue(&PasarelaDePagos::procesarTransaccion, &pasarela, transaccion8);
+
     
-    future.get();
-    future2.get();
-    future3.get();
-    future4.get();
-    future5.get();
-    future6.get();
-    future7.get();
-    future8.get();
-    */
-    int nroTransacciones = 9;
     for (int i = 0; i < nroTransacciones; i++)
     {
         Transaccion transaccion{i+1, 10.99, "USD"};
@@ -236,6 +230,10 @@ int main()
     pool.shutdown();
     std::cout<<"la execucion ha terminado!"<<std::endl;
     logger.log("---Fin de Ejecucion de Transacciones---");
-    logger.logTimeElapsed("Procesamiento de Transacciones", startTime);
+    logger.log("Nro. de Threads: " + std::to_string(nroThreads));
+    logger.log("Nro. Total de Transacciones atendidas: " + std::to_string(nroTransacciones));    
+    auto tiempoTotal = logger.logTimeElapsed("Procesamiento de Transacciones", startTime);
+    auto avgTransaccion = tiempoTotal / nroTransacciones;
+    logger.log("Tiempo Promedio por Transaccion:" + std::to_string(avgTransaccion) + " ms");
     return 0;
 }
---
+++ monitor_windows.ps1
@@ -0,0 +1,17 @@
+﻿
+# Obtener la cantidad total de memoria RAM
+$totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
+
+# Monitorear el proceso "pasarela5.exe"
+while($true) {
+  $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+  $process = Get-Process -Name "pasarela5" -ErrorAction SilentlyContinue
+  if ($process) {
+    $cpuTime = $process.CPU
+    $memUsage = $process.WorkingSet64 / 1048576
+    $date + ' > Proceso "pasarela5.exe" está en ejecución. CPU: ' + $cpuTime.ToString("#,0.000") + '%, Mem. usage: ' + $memUsage.ToString("N0") + 'MB'
+  } else {
+    $date + ' > Proceso "pasarela5.exe" no está en ejecución'
+  }
+  Start-Sleep -s 2
+}
